b4769eacf667d15860a90c0636491b188c2fe656
Jeane Hong <jeane@degreed.com>
2021-04-22 14:33:51 -0500

    m3 s7

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class QuizQuestion extends Component {
+  render() {
+    return (
+      <li>
+        <button></button>
+      </li>
+    );
+  }
+}
+
+export default QuizQuestion;
