80d23ccb162305f1ebe4a043e888380f2f8cefc7
Jeane Hong <jeane@degreed.com>
2021-04-22 14:40:11 -0500

    fix

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-class QuizQuestion extends Component {
+class QuizQuestionButton extends Component {
   render() {
     return (
       <li>
@@ -10,4 +10,4 @@ class QuizQuestion extends Component {
   }
 }
 
-export default QuizQuestion;
+export default QuizQuestionButton;
